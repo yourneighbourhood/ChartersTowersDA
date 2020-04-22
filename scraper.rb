617a146817d075b03163edd173dc1bba8cb0d34a
Your Neighbourhood <yourneighbourhood@users.noreply.github.com>
2020-04-22 22:35:18 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,31 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
+require 'date'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+url = 'http://www.charterstowers.qld.gov.au/recently-submitted-applications'
+agent = Mechanize.new
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+page = agent.get url
+
+table = page.search(:table).last
+
+table.search(:tr).each_with_index do |row,number|
+  next if number == 0 # Skip header
+
+  columns = row.search(:td)
+
+
+  date_received = Date.strptime(columns[1].inner_text.strip, '%d/%m/%Y') rescue nil
+  
+  record = {
+    council_reference: columns[0].inner_text,
+    date_received:     date_received,
+    description:       columns[3].inner_text,
+    address:           "#{columns[4].inner_text}, QLD",
+    info_url:          url,
+    comment_url:       'mailto:mail@charterstowers.qld.gov.au', 
+    date_scraped:      Date.today
+  }
+  # puts record.to_yaml
+  ScraperWiki.save_sqlite([:council_reference], record)
+end
